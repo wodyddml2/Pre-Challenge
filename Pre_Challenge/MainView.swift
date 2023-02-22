d0bf8da0e627d7a8a2c84b3078d2b16b498e1d84
wodyddml2 <wodyddml4@naver.com>
2023-02-22 13:48:58 +0900

    Chore: MainView 생성

---
+++ Pre_Challenge/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  Pre_Challenge
+//
+//  Created by J on 2023/02/22.
+//
+
+import UIKit
+
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
