ea25b9a1f0e278d4dabd4e7e8106afcd03040e6c
wodyddml2 <wodyddml4@naver.com>
2023-02-22 13:39:27 +0900

    Chore: Main 스토리보드 제거, ViewController Root View로 설정

---
+++ Pre_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre_Challenge
+//
+//  Created by J on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
